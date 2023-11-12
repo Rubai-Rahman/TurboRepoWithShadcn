a25078627307d0a5a790eaed0ac6f78fd0d2872f
Rubai Rahman <rubairahman1@gmail.com>
2023-11-12 13:07:13 +0600

    first commit

---
+++ packages/ui/components/shared/Card/TestCard.tsx
@@ -0,0 +1,80 @@
+'use client';
+import React from 'react';
+import { Checkbox } from '../../shadcn/checkbox';
+import { Button } from '../../shadcn/button';
+import { Badge } from '../../shadcn/badge';
+import { Label } from '../../shadcn/label';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../../shadcn/card';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../../shadcn/select';
+import { Input } from '../../shadcn/input';
+
+function TestCard() {
+  return (
+    <div className="bg-black h-64">
+      <h3 className="text-red-900 text-xl bg-indigo-600 ">
+        hello this is TestCard
+      </h3>
+      <div className="flex items-center space-x-2">
+        <Button>Button</Button>
+        <Badge>Badge</Badge>
+        <div className="flex items-center space-x-2">
+          <Checkbox id="terms" />
+          <Label htmlFor="terms">Accept terms and conditions</Label>
+        </div>
+        <div>
+          <Card className="w-[350px]">
+            <CardHeader>
+              <CardTitle>Create project</CardTitle>
+              <CardDescription>
+                Deploy your new project in one-click.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" placeholder="Name of your project" />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="framework">Framework</Label>
+                    <Select>
+                      <SelectTrigger id="framework">
+                        <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent position="popper">
+                        <SelectItem value="next">Next.js</SelectItem>
+                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                        <SelectItem value="astro">Astro</SelectItem>
+                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline">Cancel</Button>
+              <Button>Deploy</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestCard;
